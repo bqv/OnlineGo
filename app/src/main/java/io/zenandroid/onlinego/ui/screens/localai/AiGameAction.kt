06f4d91bdac376740a9f0e5db45aca72901abd53
Tony Olagbaiye <me@fron.io>
2021-11-14 21:49:01 +0000

    handicap and rules for sgf loading

--- app/src/main/java/io/zenandroid/onlinego/ui/screens/localai/AiGameAction.kt
+++ app/src/main/java/io/zenandroid/onlinego/ui/screens/localai/AiGameAction.kt
@@ -2,9 +2,10 @@ package io.zenandroid.onlinego.ui.screens.localai
 
 import android.graphics.Point
 import io.zenandroid.onlinego.data.model.Position
+import io.zenandroid.onlinego.data.model.local.SgfData
 
 sealed class AiGameAction {
-    class ViewReady(val loadPos: Position? = null): AiGameAction()
+    class ViewReady(val loadData: SgfData?): AiGameAction()
     object ViewPaused: AiGameAction()
     class RestoredState(val state: AiGameState): AiGameAction()
 
