e5762d7807431a2516efac1e978c3720557c9709
bqv <bqv@fron.io>
2023-10-13 17:35:28 +0100

    wip histogram

--- app/src/main/java/io/zenandroid/onlinego/ui/screens/localai/AiGameViewModel.kt
+++ app/src/main/java/io/zenandroid/onlinego/ui/screens/localai/AiGameViewModel.kt
@@ -1,13 +1,34 @@
 package io.zenandroid.onlinego.ui.screens.localai
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.reactivex.disposables.Disposable
 import io.zenandroid.onlinego.mvi.MviView
 import io.zenandroid.onlinego.mvi.Store
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.runBlocking
 
 class AiGameViewModel(private val store: Store<AiGameState, AiGameAction>): ViewModel() {
     private val wiring = store.wire()
     private var viewBinding: Disposable? = null
+    private val sharedFlow = MutableSharedFlow<AiGameState>()
+    lateinit var stateFlow: StateFlow<AiGameState>
+        private set
+
+    init {
+        runBlocking { 
+            stateFlow = sharedFlow.stateIn(viewModelScope)
+        }
+    }
+
+    fun emit(state: AiGameState) {
+        runBlocking {
+            sharedFlow.emit(state)
+        }
+    }
 
     override fun onCleared() {
         wiring.dispose()
