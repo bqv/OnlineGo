9191f859ab1576194234ae26742ce5391cb2e37c
bqv <bqv@fron.io>
2023-10-21 20:28:25 +0100

    fixup aisgf

--- app/src/main/java/io/zenandroid/onlinego/ui/screens/main/MainActivity.kt
+++ app/src/main/java/io/zenandroid/onlinego/ui/screens/main/MainActivity.kt
@@ -14,7 +14,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.os.Looper
 import android.provider.Settings
 import android.util.Log
